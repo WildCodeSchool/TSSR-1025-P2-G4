5871b603820a7ab42bd2d122c79653e554e4474a
Romii21 <romain.genoud@gmail.com>
2025-11-27 10:45:10 +0100

    creation de script

---
+++ Scripts/menu_serveur.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Preparation des fonctions
+
+Linux() {
+
+    # Connexion à la machine Linux
+    echo "Connexion à la machine Linux... "
+    echo
+    echo " ---------------------------------------------- "
+    ./menu_linux.sh
+    # local ssh $USERNAME@$HOSTNAME
+
+}
+
+Windows() {
+
+    # Connexion à la machine Windows
+    echo "Connexion à la machine Windows... "
+    echo
+    echo " ---------------------------------------------- "
+    #./menu_windows.sh
+    # local ssh $USERNAME@$HOSTNAME
+
+}
+
+# Création d'une petite interface graphique 
+clear
+echo
+echo "###############################################"
+echo "###############################################"
+echo "####                                       ####"
+echo "####                                       ####"
+echo "####             Menu serveur              ####"
+echo "####                                       ####"
+echo "####                                       ####"
+echo "###############################################"
+echo "###############################################"
+echo 
+
+while true
+do 
+
+    # Choix de la machine 
+    echo "Chossissez dans quel machine client vous voulez aller. "
+    echo
+    echo "1) Linux "
+    echo "2) Windows "
+    echo "x) Sortir "
+    echo
+    read -p "Votre choix : " client
+    echo
+
+    case $client in
+
+        1)
+            echo "Machine Linux "
+            echo
+            Linux
+            #./menu_linux.sh
+            ;;
+        
+        2)
+            echo "Machine Windows "
+            echo
+            Windows
+            #./menu_windows.sh
+            ;;
+
+        x|X)
+            echo "Au revoir "
+            echo
+            exit 0
+            ;;
+
+        *)
+            echo "Choix invalide ! "
+            echo
+            echo " ---------------------------------------------- "
+            echo
+            ;;
+
+    esac
+done
