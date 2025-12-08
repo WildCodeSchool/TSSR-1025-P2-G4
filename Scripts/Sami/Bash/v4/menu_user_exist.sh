eb641909c80455597d2925eb262c55e97be6cfb4
Ascherithub <boubakersamii@yahoo.com>
2025-12-08 01:15:42 +0100

    Ajout script gestion_repertory + modifs scripts user

---
+++ Scripts/Sami/Bash/v4/menu_user_exist.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+while true
+do
+    sleep 3
+    clear
+    echo -e "Bienvenue dans l'espace Personnel Utilisateur !\n"
+    echo -e "Que souhaitez-vous faire ?\n"
+    echo -e "1 - Apporter des modifications à l'utilisateur $user_name.\n2 - Supprimer l'utilisateur $user_name.\n3 - Afficher des Infos sur l'utilisateur $user_name .\n4 - Retourner au Menu Gestion des Utilisateurs.\nX - Sortie.\n"
+    read -p "Votre choix : " choice_menu_user_exist
+    case $choice_menu_user_exist in
+        1)
+            echo -e "\nRedirection vers l'Espace Modification Utilisateur...\n"
+            source modif_user.sh
+            continue
+        ;;
+        
+        2)
+            echo -e "\nRedirection vers l'Espace Supression Utilisateur...\n"
+            source del_user.sh
+            return
+        ;;
+        
+        3)
+            echo -e "\nRedirection vers l'Espace Informations de l'Utilisateur...\n"
+            source info_user.sh
+            continue
+        ;;
+        
+        4)
+            echo -e "\nRetour au Menu Gestion des Utilisateurs...\n"
+            return
+        ;;
+        
+        x|X)
+            echo -e "\nA bientôt !\n"
+            exit 0
+        ;;
+        
+        *)
+            clear
+            echo -e "\nErreur de saisie.\nVeuillez faire votre choix selon ce qui est proposé."
+            continue
+        ;;
+    esac
+done
---
+++ Scripts/Sami/Bash/v4/modif_user.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+function end_user_return()
+{
+    while true
+    do
+        sleep 3
+        clear
+        echo -e "Voulez-vous retourner au menu précédent ou sortir du script ?\n"
+        echo -e "1 - Retour dans l'Espace Modification Utilisateur.\nX - Sortir.\n"
+        read -p "Votre choix : " choice_return_end
+        case "$choice_return_end" in
+            1)
+                echo -e "\nRetour dans l'Espace Modification Utilisateur..."
+                break
+            ;;
+                                
+            x|X)
+                echo -e "\nA bientôt !\n"
+                exit 0
+            ;;
+                                
+            *)
+                clear
+                echo -e "\nErreur de saisie.\nVeuillez faire votre choix selon ce qui est proposé."
+                continue
+            ;;
+        esac
+    done
+}
+
+while true
+do
+    sleep 3
+    clear
+    echo "Bienvenue dans l'Espace Modification de l'Utilisateur !"
+    echo -e "\nQuelles modifications voulez-vous apporter à l'utilisateur $user_name ?\n\n1 - Modifier le mot de passe.\n2 - Ajouter $user_name au groupe administrateur.\n3 - Ajouter $user_name à un groupe local.\n4 - Retourner dans l'Espace Personnel Utilisateur ?\nX - Sortir.\n"
+    read -p "Votre choix : " choice_modif_user
+    case $choice_modif_user in
+        1)
+            passwd "$user_name"
+            echo -e "\nMot de passe modifié pour $user_name avec succès !\n"
+            end_user_return
+            continue
+        ;;
+        
+        2)
+            sudo usermod -aG sudo "$user_name"
+            echo -e "\nL'utilisateur $user_name a été ajouté au groupe administrateur avec succès !\n"
+            end_user_return
+            continue
+        ;;
+        
+        3)
+            clear
+            echo -e "\nDéfilez avec la flèche du bas ꜜ pour repérer dans quel groupe vous voulez être ajouté et continuer jusqu'à la fin de la liste pour le saisir."
+            sleep 4
+            echo -e "\nVoici la liste des groupes locaux existants :\n"
+            sleep 3
+            awk -F":" '{print $1}' /etc/group | sort | more
+            echo ""
+            
+            while true
+            do
+                read -p "Dans quel groupe existant ci-dessus souhaitez-vous être ajouté ? " local_grp
+                if getent group "$local_grp" >/dev/null 2>&1
+                then
+                    usermod -aG "$local_grp" "$user_name"
+                    echo -e "\nL'utilisateur $user_name a été ajouté au groupe $local_grp avec succès !"
+                    while true
+                    do
+                        sleep 3
+                        clear
+                        echo -e "\nVoulez-vous accorder des droits administrateurs à l'utilisateur $user_name ?\n\n1 - Oui \n2 - Non\n3 - Retour dans l'Espace Modification Utilisateur.\nX - Sortir.\n"
+                        read -p "Votre choix : " mod_sudo
+                        case $mod_sudo in
+                            1)
+                                usermod -aG sudo "$user_name"
+                                echo -e "\nL'utilisateur $user_name du groupe $local_grp a aussi été ajouté au groupe administrateur avec succès !"
+                                end_user_return
+                                continue 3
+                            ;;
+                                        
+                            2)
+                                echo -e "\nL'utilisateur $user_name du groupe $local_grp n'a pas été ajouté au groupe administrateur."
+                                end_user_return
+                                continue 3
+                            ;;
+                                        
+                            3)
+                                echo -e "\nRetour dans l'Espace Modification Utilisateur..."                                        
+                                continue 3
+                            ;;
+                                        
+                            x|X)
+                                echo -e "\nA bientôt !\n"
+                                exit 0
+                            ;;
+                                        
+                            *)
+                                clear
+                                echo -e "\nErreur de saisie.\nVeuillez faire votre choix selon ce qui est proposé."
+                                continue
+                            ;;
+                        esac
+                    done
+                else
+                    clear
+                    echo -e "\nLe groupe $local_grp n'existe pas. Veuiller réessayer SVP.\n"
+                    continue
+                fi
+            done
+        ;;
+
+        3)  
+            echo -e "\nRetour dans l'Espace Personnel Utilisateur..."
+            return
+        ;;
+
+        x|X)
+            echo -e "\nA bientôt !\n"
+            exit 0
+        ;;
+
+        *)
+            clear
+            echo -e "\nErreur de saisie.\nVeuillez faire votre choix selon ce qui est proposé."
+            continue
+        ;;
+    esac
+done
