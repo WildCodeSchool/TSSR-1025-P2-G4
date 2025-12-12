c1d06ab59d4c3d89f6a2a739bb80e226af200e96
Ascherithub <boubakersamii@yahoo.com>
2025-12-12 15:52:28 +0100

    Recupération des scripts avant supression des machines Proxmox

--- Scripts/Romain/debian_windows/module_1.sh
+++ Scripts/Romain/debian_windows/module_1.sh
@@ -27,7 +27,7 @@ function Feu() {
     echo " ---------------------------------------------- "
     echo
     sleep 1
-    ~/scripts_debian/windows/menu_pare-feu.sh "$NomMachine" "$IpMachine"
+    source ~/scripts_debian/windows/menu_pare-feu.sh "$NomMachine" "$IpMachine"
 
 }
 
@@ -39,7 +39,7 @@ function Redemarrer() {
     echo " ---------------------------------------------- "
     echo
     sleep 1
-    ~/scripts_debian/windows/menu_redemarrage.sh  "$NomMachine" "$IpMachine"
+    source ~/scripts_debian/windows/menu_redemarrage.sh  "$NomMachine" "$IpMachine"
 
 }
 
@@ -66,7 +66,7 @@ function Log() {
     local utilisateur=$(whoami)
 
     # Format demandé <Date>_<Heure>_<Utilisateur>_<Evenement>
-    local ligne_log="${date_actuelle}"_${heure_actuelle}_${utilisateur}_${evenement}
+    local ligne_log="${date_actuelle}_${heure_actuelle}_${utilisateur}_${evenement}"
 
     # Ecriture dans le fichier
     echo "$ligne_log" | sudo tee -a "$fichier_log" > /dev/null 2>&1
