2e999a7ef2ad7ee6cf0ff3f10bd18d1886ad8699
Romii21 <romain.genoud@gmail.com>
2025-12-16 10:14:29 +0100

    Add files via upload

---
+++ Scripts/Romain/sricpts_powershell/linux/module_1.ps1
@@ -0,0 +1,128 @@
+# Module 1 (PowerShell)
+
+# Initialisation des arguments
+
+param(
+    [string]$NomMachine,
+    [string]$IpMachine
+)
+
+# Initialisation des fonctions 
+
+function Log {
+
+
+}
+
+# Menu prise en main
+function PriseEnMain {
+    Write-Host "Connexion au menu prise en main..."
+    Write-Host ""
+    Write-Host " ---------------------------------------------- "
+    Write-Host ""
+    Start-Sleep -Seconds 1
+    
+    # Appel du script menu_prise_en_main.ps1
+    & "$PSScriptRoot\linux\menu_prise_en_main.ps1" -NomMachine $NomMachine -IpMachine $IpMachine
+}
+
+# Menu pare-feu
+function PareFeu {
+    Write-Host "Connexion au menu pare-feu..."
+    Write-Host ""
+    Write-Host " ---------------------------------------------- "
+    Write-Host ""
+    Start-Sleep -Seconds 1
+    
+    # Appel du script menu_pare-feu.ps1
+    & "$PSScriptRoot\linux\menu_pare-feu.ps1" -NomMachine $NomMachine -IpMachine $IpMachine
+}
+
+# Menu redémarrage
+function Redemarrage {
+    Write-Host "Connexion au menu rdémarrage..."
+    Write-Host ""
+    Write-Host " ---------------------------------------------- "
+    Write-Host ""
+    Start-Sleep -Seconds 1
+    
+    # Appel du script menu_redemarrage.ps1
+    & "$PSScriptRoot\linux\menu_redemarrage.ps1" -NomMachine $NomMachine -IpMachine $IpMachine
+}
+
+
+Log "NewScript"
+
+# Boucle principale du menu
+while ($true) {
+    Clear-Host
+    Write-Host ""
+    Write-Host "###############################################"
+    Write-Host "###############################################"
+    Write-Host "####                                       ####"
+    Write-Host "####                                       ####"
+    Write-Host "####          Menu Action Machine          ####"
+    Write-Host ("####  {0,-37}  ####" -f "$NomMachine $IpMachine")
+    Write-Host "####                                       ####"
+    Write-Host "###############################################"
+    Write-Host "###############################################"
+    Write-Host ""
+
+    # Choix du module
+    Write-Host "Choisissez dans quel module vous voulez aller."
+    Write-Host ""
+    Write-Host "1) Menu prise en main distante"
+    Write-Host "2) Menu pare-feu"
+    Write-Host "3) Menu redémarrage"
+    Write-Host "4) Retour Menu Serveur"
+    Write-Host "x) Sortir"
+    Write-Host ""
+    $module = Read-Host "Votre choix"
+    Write-Host ""
+
+    switch ($module) {
+        "1" {
+            Write-Host "Menu prise en main distante"
+            Write-Host ""
+            PriseEnMain "$NomMachine" "$IpMachine"
+            Log "PriseEnMain"
+        }
+        "2" {
+            Write-Host "Menu pare-feu"
+            Write-Host ""
+            PareFeu "$NomMachine" "$IpMachine"
+            Log "PareFeu"
+        }
+        "3" {
+            Write-Host "Menu redémarrage"
+            Write-Host ""
+            Redemarrage "$NomMachine" "$IpMachine"
+            Log "MenuRedemarrage"
+        }
+        "4" {
+            Write-Host "Retour Menu Linux"
+            Write-Host ""
+            Write-Host "Connexion Menu Linux..."
+            Write-Host ""
+            Write-Host " ---------------------------------------------- "
+            Write-Host ""
+            Start-Sleep -Seconds 1
+            Log "RetourMenuLinux"
+            return
+        }
+        { $_ -ieq "x" } {
+            Write-Host "Au revoir"
+            Write-Host ""
+            Log "EndScript"
+            exit 0
+        }
+        default {
+            Write-Host "Choix invalide !"
+            Write-Host ""
+            Write-Host " ---------------------------------------------- "
+            Write-Host ""
+            Start-Sleep -Seconds 1
+            Log "MauvaisChoix"
+        }
+    }
+}
